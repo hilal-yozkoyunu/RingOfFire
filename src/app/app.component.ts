5a39e687e4c52adcfc27c65c69a477f6075fc655
Hilal Yozkoyunu <hilal_2501@hotmail.de>
2024-06-26 00:32:11 +0200

    added start-screen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { StartScreenComponent } from './start-screen/start-screen.component';
+import { GameComponent } from './game/game.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    StartScreenComponent,
+    GameComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
